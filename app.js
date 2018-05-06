d53446192c13985306aaef9293e44552c7bea7a7
Aliia Chekmareva <aliia.chekmareva@groupm.com>
2018-05-07 02:27:02 +0400

    Implemented using modules in app.js

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
-import config from  "./config/config.json"
-import {User, Product} from "./models"
+import config from "./config/config.json"
+import { User, Product } from "./models"
+import Importer from "./importer/Importer.js"
+import DirWatcher from './dirwatcher/DirWatcher.js'
 
-console.log(config.name)
-
+console.log(config.name);
 const user = new User();
 const product = new Product();
+const dirwatcher = new DirWatcher();
+dirwatcher.watch("D:/Projects/NodeMentoring/NodeMentoring/data", 1000);
+const importer = new Importer(dirwatcher);
+dirwatcher.on("changed", (path) => {
+  importer.import(path).then(response => {
+    console.log("------------------------");
+    console.log("file name: " + response.filename)
+    console.log(response.data);
+  })
+});
 
