c94b6417152fb5782bb726900464c1ea08346176
Aliia Chekmareva <aliia.chekmareva@groupm.com>
2018-05-07 02:24:29 +0400

    Added Importer

---
+++ importer/Importer.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const csvjson = require('csvjson')
+
+class Importer {
+  import(path) {
+    console.log("async importing");
+    return new Promise((resolve, reject) => {
+      fs.readFile(path, { encoding: 'utf8' }, (err, data) => {
+        if (err) {
+          reject(err);
+          return;
+        };
+        let json = this.convertToJson(data);
+        resolve({ filename: path, data: json });
+      });
+    });
+  }
+
+  importSync(path) {
+    console.log("sync importing");
+    let data = fs.readFileSync(path, { encoding: 'utf8' });
+    return ({ filename: path, data: this.convertToJson(data) });
+  }
+
+  convertToJson(data) {
+    let options = {
+      delimiter: ',',
+      quote: '"'
+    };
+    return csvjson.toObject(data, options);
+  }
+}
+
+export default Importer
