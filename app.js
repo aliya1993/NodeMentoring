21de29a3cd2aff927f5ec62cf655c346b4dc544e
Aliia Chekmareva <aliia.chekmareva@groupm.com>
2018-05-09 23:27:40 +0400

    Updated app.js

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ console.log(config.name);
 const user = new User();
 const product = new Product();
 const dirwatcher = new DirWatcher();
-dirwatcher.watch("D:/Projects/NodeMentoring/NodeMentoring/data", 1000);
+dirwatcher.watch("D:/Projects/NodeMentoring/NodeMentoring/data", 5000);
 const importer = new Importer(dirwatcher);
-dirwatcher.on("changed", (path) => {
-  importer.import(path).then(response => {
-    console.log("------------------------");
-    console.log("file name: " + response.filename)
-    console.log(response.data);
-  })
-});
+// dirwatcher.on("changed", (path) => {
+//   importer.import(path).then(response => {
+//     console.log("------------------------");
+//     console.log("file name: " + response.filename)
+//     console.log(response.data);
+//   })
+// });
 
