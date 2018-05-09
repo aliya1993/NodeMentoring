4a55152e6e2b01066c4c2447269e20da97009664
Aliia Chekmareva <aliia.chekmareva@groupm.com>
2018-05-10 02:21:39 +0400

    Fixed implementation for sync and async methods

--- app.js
+++ app.js
@@ -2,14 +2,6 @@ import config from "./config/config.json";
 import { User, Product } from "./models";
 import Importer from "./importer/Importer.js";
 import DirWatcher from './dirwatcher/DirWatcher.js';
-const readline = require('readline');
-
-const input = process.stdin;
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
 
 console.log(config.name);
 
@@ -19,28 +11,20 @@ const product = new Product();
 const dirwatcher = new DirWatcher(500);
 const importer = new Importer(dirwatcher);
 
-const startImporter = (numberOfFiles) => {
-  let count = 0;
-  importer.on('done', (result) => {
-    console.log('done');
-
-    result.then((response) => {
-      console.log('------------------------');
-      console.log('file name: ' + response.filename)
-      console.log(response.data)
-    });
-    if (count >= numberOfFiles - 1) {
-      importer.stopImporting();
-      input.removeAllListeners('data');
-      rl.write('Uploading has been finished\r')
-    }
-    count++;
+importer.on('done', (result) => {
+  console.log('done');
+  result.then((response) => {
+    console.log('------------------------');
+    console.log('file name: ' + response.filename)
+    console.log(response.data)
   });
-  importer.startImporting(`${__dirname}\\data`, 5000);
-}
-
-rl.write('Enter number of files to upload\r');
-input.on('data', (numberOfFiles) => {
-  console.log(`Received: ${numberOfFiles}`);
-  startImporter(numberOfFiles);
 });
+
+setTimeout(function () {
+  importer.startImporting(`${__dirname}\\data`, 5000);
+}, 60000);
+importer.stopImporting();
+
+importer.import(`${__dirname}\\data\\File1.csv`).then(res => { console.log('Predefined async:'); console.log(res) });;
+console.log('Predefined sync:');
+console.log(importer.importSync(`${__dirname}\\data\\File1.csv`));
